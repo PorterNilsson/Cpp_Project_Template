7a0f7244e8a76a11ba356036aa1cfe9a75c46ca3
PorterNilsson <92951219+PorterNilsson@users.noreply.github.com>
2024-03-27 09:23:47 -0600

    Made main.cpp more readable

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
 int main(int, char**) {
-    std::cout << "Welcome to your new c++ project!" << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "################################" << std::endl;
+    std::cout << "Welcome to your new C++ project!" << std::endl;
+    std::cout << "################################" << std::endl;
+    std::cout << std::endl;
+    
 }
